4f18b989f6c667f0480496db255abcfd638a050d
jbresky <joaquin_bresky@hotmail.com>
2022-10-17 18:44:30 -0300

    fix no-unescaped-entities

--- components/EmptyState.js
+++ components/EmptyState.js
@@ -11,9 +11,9 @@ const EmptyState = () => (
     <DashboardShell>
          <Box backgroundColor="white" width="100%" borderRadius={4} p={8}>
             <Heading size="md" as="h2">
-              You haven't added any sites
+              You haven&apost added any sites
             </Heading>
-            <Text>Welcome! Let's get started,</Text>
+            <Text>Welcome! Let&aposs get started,</Text>
             <Button variant="solid" size="md">
               Add your First Site
             </Button>
