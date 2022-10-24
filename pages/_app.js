ab3df1901e68a7530eb1a01543a5028101a660b6
jbresky <joaquin_bresky@hotmail.com>
2022-10-24 17:06:23 -0300

    fetch feedback data from firestore

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,11 @@
 import '../styles/globals.css'
 import { AuthProvider } from '../lib/auth'
-// import { ThemeProvider } from '@emotion/react'
-// import { CSSReset } from '@chakra-ui/react'
 import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }) {
   return (
-    // <ThemeProvider theme={theme}>
     <ChakraProvider>
       <AuthProvider>
-        {/* <CSSReset/> */}
         <Component {...pageProps} />
       </AuthProvider>
     </ChakraProvider>
